1f245ac1ddc950185d3a30e26ac02e6820557911
tanu <tanu123@gmail.com>
2024-07-05 23:54:17 -0400

    change2

--- one.java
+++ one.java
@@ -2,7 +2,9 @@ public class one
 {
 	public static void main (String [] args)
 	{
-		system.out.println("hello tumkur");
+            system.out.println("hello tumkur");
+	    system.out.println("mysore");
 	}
+
 }
 
