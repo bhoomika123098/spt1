5fed8447adab21423dc7fcb71de37b524db1f51d
tanu <tanu123@gmail.com>
2024-07-05 23:58:28 -0400

    change3

--- one.java
+++ one.java
@@ -3,7 +3,7 @@ public class one
 	public static void main (String [] args)
 	{
             system.out.println("hello tumkur");
-	    system.out.println("mysore");
+	    system.out.println("mysore,gubbi");
 	}
 
 }
